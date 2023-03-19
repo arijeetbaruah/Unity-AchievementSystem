9b7c4c93e40d8688adcee9e92cfd8bd7a399ca8b
arijeet baruah <arijeetbaruah@gmail.com>
2023-03-19 23:25:16 +0530

    Player Database

--- Assets/Games/Scripts/Combat/CharacterDatabase.cs
+++ Assets/Games/Scripts/Combat/CharacterDatabase.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 using Sirenix.OdinInspector;
 using Sirenix.Utilities;
+using System.Linq;
 
 [GlobalConfig, CreateAssetMenu]
 public class CharacterDatabase : GlobalConfig<CharacterDatabase> 
 {
     public List<CharacterDetails> characters;
+
+    [ReadOnly, ShowInInspector]
+    public SerializedCharacterDetailsDictionary characterDatabase => new SerializedCharacterDetailsDictionary(characters.ToDictionary(c => c.characterID));
+}
+
+[System.Serializable]
+public class SerializedCharacterDetailsDictionary : UnitySerializedDictionary<string, CharacterDetails>
+{
+    public SerializedCharacterDetailsDictionary() : base() { }
+    public SerializedCharacterDetailsDictionary(Dictionary<string, CharacterDetails> dic) : base(dic) { }
 }
